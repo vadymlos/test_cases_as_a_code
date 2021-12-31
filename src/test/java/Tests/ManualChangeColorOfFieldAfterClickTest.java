005671d7bd275a84d04bd5b03819a41725723bcf
vadymlos <vadym.los@pdffiller.team>
2021-12-31 17:57:08 +0200

    add second test

---
+++ src/test/java/Tests/ManualChangeColorOfFieldAfterClickTest.java
@@ -0,0 +1,20 @@
+package Tests;
+
+import io.qameta.allure.*;
+import org.testng.annotations.Test;
+
+public class ManualChangeColorOfFieldAfterClickTest {
+    @Feature("Fillable fields")
+    @Component("Fields list")
+    @Story("")
+    @Owner("Andrey Fiyas")
+    @Manual()
+    @Tag("CONSTRUCTOR")
+    @Test(description = "Field changes color to active after click")
+    @AllureId("47512")
+    public static void field_changes_color_to_active_after_click() {
+        Allure.step("Open constructor");
+        Allure.step("Click on text field and check color of item in fields list");
+        Allure.step("Add this field to document");
+    }
+}
