0c4f61b81bfae8b6e8f4ed475f4daa26208439c0
vadymlos <vadym.los@pdffiller.team>
2022-01-04 16:12:28 +0200

    add opened fields list test

--- src/test/java/test/cases/fields/list/ManualFieldListMenuOpenedTest.java
+++ src/test/java/test/cases/fields/list/ManualFieldListMenuOpenedTest.java
@@ -6,19 +6,21 @@ import test.cases.Component;
 import test.cases.Manual;
 import test.cases.Tag;
 
-public class ManualFieldsListMenuCollapsedTest {
+public class ManualFieldListMenuOpenedTest {
     @Feature("Fillable fields")
     @Component("Fields list")
     @Story("")
     @Owner("Andrey Fiyas")
     @Manual()
     @Tag("CONSTRUCTOR")
-    @Test(description = "Fields list menu collapsed after click on the title")
-    @AllureId("47513")
-    public static void field_list_menu_collapsed_after_on_the_title() {
+    @Test(description = "Fields list menu opened after click on the title")
+    @AllureId("47514")
+    public static void field_list_menu_opened_after_on_the_title() {
         Allure.step("Open constructor");
         Allure.step("Click on tab 'Fields'");
         Allure.step("Click on title in fields list");
         Allure.step("Check that list of fields is collapsed");
+        Allure.step("Click on title in fields list");
+        Allure.step("Check that list of fields is opened");
     }
 }
